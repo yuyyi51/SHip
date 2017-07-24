82930664940121c06a0d29de7a8ea1c76c243317
suiguotou <513340952@qq.com>
2017-07-24 13:21:19 +0800

    ...

---
+++ Assets/Script/HpUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HpUI : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Slider>().maxValue = gameObject.transform.parent.transform.parent.GetComponent<Ship>().health;
+		gameObject.GetComponent<Slider>().value = gameObject.transform.parent.transform.parent.GetComponent<Ship>().health;
+		Debug.Log (gameObject.GetComponent<Slider> ().value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+		Debug.Log (gameObject.GetComponent<Slider> ().value);
+		gameObject.GetComponent<Slider>().value = gameObject.transform.parent.transform.parent.GetComponent<Ship>().health;
+	}
+}
---
+++ Assets/Script/Ship.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour 
+{
+
+	public GameObject explode;
+
+	public float health;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//Debug.Log ("triggered");
+
+		if (other.GetComponent<is_en_tag>().is_enemy != gameObject.GetComponent<is_en_tag>().is_enemy) {
+
+
+			float damage = 0;
+
+			if (other.GetComponent<Missile> () != null)
+				damage = other.GetComponent<Missile> ().damage;
+			if (other.GetComponent<Shell> () != null)
+				damage = other.GetComponent<Shell> ().damage;
+
+
+			health -= damage;
+
+			if (health <= 0) {
+
+				explode.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f); 
+
+				Instantiate (explode, gameObject.transform.position, gameObject.transform.rotation);
+
+				Destroy (gameObject);
+
+
+			}
+
+
+		}
+
+
+	}
+}
