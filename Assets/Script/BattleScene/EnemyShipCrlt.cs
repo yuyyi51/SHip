74158ea18713d9b39f2432e227f79fe4aeeb797c
suiguotou <513340952@qq.com>
2017-07-25 19:58:03 +0800

    完善了开船的脚本

--- Assets/Script/BattleScene/EnemyShipCrlt.cs
+++ Assets/Script/BattleScene/EnemyShipCrlt.cs
@@ -8,7 +8,13 @@ public class EnemyShipCrlt : MonoBehaviour {
 
 	public float safeDis;
 
+	public float turnSpd;			//转向速度
+	public float accel;          	//加速度
+	public float astern;        	//后退加速度
+	public float maxSpeed;        	//最大速度
+	public float maxAstSpeed;  		//最大后退速度
 
+	public float spd;
 
 	// Use this for initialization
 	void Start () {
@@ -30,19 +36,34 @@ public class EnemyShipCrlt : MonoBehaviour {
 
 	}
 
-	void move(Vector3 pos){
-		
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
 
-		const GameObject en = BattleController.instance.FindClosestEnemy (gameObject.transform.position, 10000f, !gameObject.GetComponent<is_en_tag> ().is_enemy);//10000f代指正无穷
+		GameObject en = BattleController.instance.FindClosestEnemy (gameObject.transform.position, 10000f, !gameObject.GetComponent<is_en_tag> ().is_enemy);//10000f代指正无穷
 
 		if ((en.transform.position - gameObject.transform.position).magnitude > range [0]) { // 炮的射程按照从远到近排列
 
-			
+			Tools.TurnTowards (gameObject, en, turnSpd);
 
+			if (spd + accel <= maxSpeed) {
+				spd += accel;
+			}
+
+			transform.Translate(Vector3.up * spd * Time.deltaTime);
+
+
+
+		}
+
+		if ((en.transform.position - gameObject.transform.position).magnitude < safeDis) { 
+
+			if (spd - astern >= maxAstSpeed) {
+				spd -= astern;
+			}   
+
+			transform.Translate (Vector3.up * spd * Time.deltaTime);
 		}
 	}
 }
--- Assets/Script/BattleScene/Tools.cs
+++ Assets/Script/BattleScene/Tools.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Tools
 {
-    static void TurnTowards(GameObject obj, Vector3 target, float vel)
+    public static void TurnTowards(GameObject obj, Vector3 target, float vel)
     {
         Vector3 dir = target - obj.transform.position;
         float angle = Vector3.Angle(obj.transform.up, dir);
         float d = (Vector3.Dot(Vector3.forward, Vector3.Cross(obj.transform.up, dir)) < 0 ? -1 : 1);
         obj.transform.Rotate(0, 0, d * vel * Time.deltaTime);
     }
-    static void TurnTowards(GameObject obj, GameObject target, float vel)
+	public static void TurnTowards(GameObject obj, GameObject target, float vel)
     {
         TurnTowards(obj, target.transform.position, vel);
     }
