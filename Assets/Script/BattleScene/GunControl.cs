bccc0cb36247554bd96f7bb96339c9d37d10084a
yuyyi51 <yuyyi51@126.com>
2017-07-26 17:02:29 +0800

    补上了遗漏的文件

---
+++ Assets/Script/BattleScene/GunControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        target.z = 0f;
+        target.Normalize();
+        float euler = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg - 90f;
+        Quaternion TargetRotation = Quaternion.Euler(0, 0, euler);
+        Quaternion re = Quaternion.Slerp(transform.rotation, TargetRotation, Time.deltaTime * speed);
+        transform.rotation = re;
+    }
+}
---
+++ Assets/Script/BattleScene/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject[] muzzle;
+    public CDTime cd;
+    public float turningSpeed;
+
+
+
+    abstract public void Fire(GameObject obj, Vector3 point, GameObject target = null);
+    public void TurnTowards(Vector3 target)
+    {
+        Tools.TurnTowards(gameObject, target, turningSpeed);
+    }
+    public void TurnLeft()
+    {
+        Tools.TurnLeft(gameObject, turningSpeed);
+    }
+    public void TurnRight()
+    {
+        Tools.TurnRight(gameObject, turningSpeed);
+    }
+}
