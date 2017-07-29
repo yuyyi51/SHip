0053ec2a909fd7b8660826f3436a0d5f796f1250
yuyyi51 <yuyyi51@126.com>
2017-07-29 21:49:48 +0800

    修正了Tools类中提供的转向目标的函数会出现物件抖动的问题

--- Assets/Script/BattleScene/Tools.cs
+++ Assets/Script/BattleScene/Tools.cs
@@ -7,12 +7,13 @@ public class Tools
     public static void TurnTowards(GameObject obj, Vector3 target, float vel)
     {
         Vector3 dir = target - obj.transform.position;
+        dir.z = 0;
         float angle = Vector3.Angle(obj.transform.up, dir);
         float d = (Vector3.Dot(Vector3.forward, Vector3.Cross(obj.transform.up, dir)) < 0 ? -1 : 1);
         if (angle > vel * Time.deltaTime)
             obj.transform.Rotate(0, 0, d * vel * Time.deltaTime);
         else
-            obj.transform.Rotate(0, 0, angle);
+            obj.transform.Rotate(0, 0, d * angle);
     }
     public static void TurnTowards(GameObject obj, GameObject target, float vel)
     {
