83f46e049b2eb72e65981fc7725608620d3626c4
yuyyi51 <yuyyi51@126.com>
2017-07-31 22:16:31 +0800

    加入了新武器的prefab和子弹脚本

--- Assets/Script/BattleScene/Weapon/FrozOrb.cs
+++ Assets/Script/BattleScene/Weapon/FrozOrb.cs
@@ -7,7 +7,21 @@ public class FrozOrb : Weapon {
     // Use this for initialization
     override public void Fire(GameObject obj, Vector3 point, GameObject target)
     {
-
+        if (!cd.ColdDownFinished())
+        {
+            return;
+        }
+        Vector3 v1 = point - transform.position;
+        foreach (GameObject muzz in muzzle)
+        {
+            Vector3 v2 = muzz.transform.position - transform.position;
+            Vector3 tar = v1 - v2;
+            tar.z = 0;
+            float range = tar.magnitude;
+            GameObject bull = Instantiate(bullet, muzz.transform.position, muzz.transform.rotation);
+            bull.GetComponent<Shell>().Initial(range, obj.GetComponent<Ship>().Speed, obj.transform.up, GetComponentInParent<is_en_tag>().is_enemy);
+        }
+        cd.StartColdDown();
     }
 
     // Update is called once per frame
